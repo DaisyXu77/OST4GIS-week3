fb16adeb4cc166052f5a452cd7f818eb70de7c58
Shuhan <daisy_xsh@163.com>
2018-01-26 16:07:36 -0500

    lab 1-part1&2 finished
    change all results to 'true'

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,7 @@ Functions can be passed as values to other functions. Each exercise here builds 
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function(whatever) {return 1;};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +15,7 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(num) {return num%2===0;};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +24,7 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function(whatever2) {return isEven(justOne(whatever2));};
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,16 +32,19 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(trueorfalse) {
+  if (trueorfalse==true){return false;
+  }else{return true;}
+};
 
-console.log('not success:', not(true) === false);
+console.log('not success:', not(false) === true);
 
 /* =====================
 Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+var isOdd = function(num) {return not(isEven(num));};
 
 console.log('isOdd success:', isOdd(4) === false);
 
@@ -49,9 +52,17 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {};
+var filterOutLessThan10 = function(list) {
+  newlist = [];
+  for(i=0;i<list.length;i++){
+    if (list[i]>10){newlist.push(list[i]);}
+  }
+  return newlist;
+};
+console.log(filterOutLessThan10([4, 11]));
+var Equal = _.isEqual(filterOutLessThan10([4, 11]),[11]);
 
-console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]) === [11]);
+console.log('filterOutLessThan10 success:', Equal);
 
 /* =====================
 Stretch goal
@@ -60,7 +71,13 @@ Instructions: Let's bring it all together. Write a function that filters a list 
               2. a function that takes a value and returns true (to keep a number) or false (to toss it out)
 ===================== */
 
-var filter = function(array, func) {};
+var filter = function(array, func) {
+  newlist = [];
+  for(j=0;j<array.length;j++){
+    if (func(array[j])==true){
+      newlist.push(array[j]);}
+  }
+  return newlist;
+};
 
-console.log('filter success:', filter([4, 11], isOdd) === [11]);
-
+console.log('filter success:', _.isEqual(filter([4, 11], isOdd),[11]));
